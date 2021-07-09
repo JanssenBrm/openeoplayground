3848b3bee645f2671f00b1967c8d1be9c0d50539
janssenb <bram.janssen@vito.be>
2021-07-09 17:00:14 +0200

    Initial integration with React and Leaflet

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import styles from './Map.module.css';
+
+const Map = () => (
+    <MapContainer className={styles.MapContainer} center={[50.7191078335712, 4.721902473674258]} zoom={8}>
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
+    </MapContainer>
+);
+
+export default Map;
